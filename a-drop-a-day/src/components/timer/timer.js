a7264d71b28e0b98b4057ce4bac7e17922eedbf7
Golden Eagle <joowoon.kang@biola.edu>
2020-06-14 21:07:00 -0700

    find a way to add timer inside and working fine without breaking values as decrements

--- a-drop-a-day/src/components/timer/timer.js
+++ a-drop-a-day/src/components/timer/timer.js
@@ -1,16 +1,80 @@
-import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+// import "./styles.css";
 
-const Timer = () => {
+export default function Timer() {
+  const [Minutes, setMinutes] = useState(25);
+  const [isRunning, setIsRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+
+  useEffect(() => {
+    if (isRunning) {
+      const id = window.setInterval(() => {
+        setMinutes((min) => min - 1);
+      }, 1000);
+
+      return () => window.clearInterval(id);
+    } else {
+    }
+  }, [isRunning]);
+  console.log({ Minutes });
+
+  // let someColor = "Blue";
+  var formattedNumber = ("0" + Minutes).slice(-2);
+
+  const styles = {
+    // backgroundColor: backgroundColor,
+    // fontSize: someSize,
+    color: "#ADFF2F",
+    // padding: paddings,
+  };
+  //  ...
+  //  <div style={styles}>
+  if (Minutes < 24) {
+    styles.color = "red";
+  }
+  if (Minutes == 0) {
+    setMinutes(25);
+    setIsRunning(false);
+  }
   return (
-    <section className="timer-container">
-      <section className="timer">
-        <div>
-          <span className="mdi mdi-calendar-clock timer-icon"></span>
-          <h2>Countdown Timer</h2>
-        </div>
-      </section>
-    </section>
+    <div>
+      <i style={styles} className="fas fa-apple-alt fa-10x" />
+
+      <br />
+      {isRunning ? <>Coutning Down</> : <>Pause</>}
+      <div className="timer-circle">
+        <div className="time">{formattedNumber} Minutes</div>
+      </div>
+      <div className="button">
+        <button
+          disabled={isRunning}
+          className="play-pause"
+          onClick={() => {
+            setIsRunning(true);
+          }}
+        >
+          <i className="fa fa-play fa-2x" />
+        </button>
+        <button
+          className="play-pause"
+          onClick={() => {
+            setIsRunning(false);
+          }}
+        >
+          <i className="fa fa-pause fa-2x" />
+        </button>
+        <button onClick={() => setMinutes(Minutes + 1)}>inc Minutes</button>
+        <button
+          onClick={() => {
+            setIsRunning(false);
+            setMinutes(25);
+          }}
+          className="reset"
+        >
+          Reset
+        </button>
+      </div>
+    </div>
   );
-};
-export default Timer;
+}
