7627f1a2aaf6410f6b71960c9f04cf6d4700d7e8
Golden Eagle <joowoon.kang@biola.edu>
2020-06-22 22:13:29 -0700

    spelling checking using microsoft office

--- a-drop-a-day/src/components/home/home.component.js
+++ a-drop-a-day/src/components/home/home.component.js
@@ -8,8 +8,8 @@ const Home = () => {
       <p>
         {" "}
         As you know there are tasks to finish in a limited amount of time and
-        also we'd agree that time is gold. Most people would also agree that the
-        more we stay focused on one thing, we finish stronger and faster.
+        also, we'd agree that time is gold. Most people would also agree that
+        the more we stay focused on one thing; we finish stronger and faster.
         However, it's also true that we tend to procrastinate even if we have
         tasks in front of us. Recently, I've been using a technique called&nbsp;
         <a
@@ -19,7 +19,7 @@ const Home = () => {
         >
           the Pomodoro Technique
         </a>
-        &nbsp;whenever I need to focus. Although this method has been immensly
+        &nbsp;whenever I need to focus. Although this method has been immensely
         helpful for me to stay focused and stay away from procrastinating, I
         haven't found a website where I can break down on things I was working
         on.
@@ -38,8 +38,8 @@ const Home = () => {
             Plato
           </li>
         </ul>
-        As the to-do list shows above, I implmeneted a feature where I can set
-        how much time I will spend a day for each task. So the list would look
+        As the to-do list shows above, I implemented a feature where I can set
+        how much time I will spend a day for each task. So, the list would look
         like this.
         <ul>
           <li>
@@ -48,7 +48,7 @@ const Home = () => {
           </li>
           <li>
             Solving algorithm problems on Leetcode, two easy problems and one
-            medium level probelm -->
+            medium level problem -->
             <span style={{ color: "tomato" }}> 4 tomatoes </span> a day
           </li>
           <li>
@@ -57,18 +57,12 @@ const Home = () => {
           </li>
         </ul>
         Wait, what's does it mean by tomatoes? Here's the answer, each tomato
-        will be a time tracker. One tomato will be 25 mintues. Taking a short 5
-        minute break time after a tomato would be recommended for our brains.{" "}
-        <a
-          href="https://www.linkedin.com/in/joo-woon-kang-2515ab1a2/"
-          target="blank"
-        >
-          Taking a break time can be helfup for us
-        </a>{" "}
-        to think more creative way. After a first four tomatoes, taking a long
-        15 minutes break time is also recommended. Once you logged in this
-        website, you will be able to make a to-do list with numbers of tomatoes
-        you wish to work on.
+        will be a time tracker. One tomato will be 25 minutes. Taking a short
+        5-minute break time after a tomato would be recommended for our brains.
+        Taking a break time can be helpful for us to think more creative way.
+        After a first four tomatoes, taking a long 15 minutes break time is also
+        recommended. Once you logged in this website, you will be able to make a
+        to-do list with numbers of tomatoes you wish to work on.
       </p>
       <h2> See this image as below:</h2>
       <p>
@@ -100,7 +94,7 @@ const Home = () => {
           here
         </a>
         . I'd love to hear your feedback and stay connected. Also if you like to
-        see this side project's repo on github, you can click&nbsp;
+        see this side project's repo on GitHub, you can click&nbsp;
         <a
           href="https://github.com/joowoonk/Side-Project-A-Drop-A-Day"
           target="blank"
@@ -109,10 +103,10 @@ const Home = () => {
         </a>
       </p>
 
-      <h1>What's The Pomodoro Technique about?</h1>
+      <h1>What's the Pomodoro Technique about?</h1>
       <p>
         It's one of the time management methods out there that's widely used.
-        You can click&nbsp;
+        You can click &nbsp;
         <a
           href="https://francescocirillo.com/pages/pomodoro-technique"
           target="blank"
@@ -139,10 +133,10 @@ const Home = () => {
         <h2>Lastly...</h2>
         As we work on things daily basis, I believe there is nothing we cannot
         master. I'm a stronger believer in compound interest where doing one
-        small thing a day will grow us expentially. I hope you dont give up on
-        whatever you are passionated about. Thank you again for taking your time
-        using this website. Oh, try to click tomato logo on top menu if you want
-        dark-mode on this website.
+        small thing a day will grow us exponentially. I hope you don’t give up
+        on whatever you are passionate about. Thank you again for taking your
+        time using this website. Oh, try to click tomato logo on top menu if you
+        want dark-mode on this website.
       </p>
     </div>
   );
