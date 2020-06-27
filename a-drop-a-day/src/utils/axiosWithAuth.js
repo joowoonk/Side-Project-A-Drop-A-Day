7f3c94265c371d1b4447bbc31ad893b8fb7b0cc9
Golden Eagle <joowoon.kang@biola.edu>
2020-06-27 00:57:51 -0700

    disabling navbars while focusing time or breaking time is on, made minutes more intuitive.

--- a-drop-a-day/src/utils/axiosWithAuth.js
+++ a-drop-a-day/src/utils/axiosWithAuth.js
@@ -5,7 +5,7 @@ export const axiosWithAuth = () => {
     headers: {
       authorization: token,
     },
-    // baseURL: "https://tomatoestimer.herokuapp.com/api/",
-    baseURL: "http://localhost:8000/api/",
+    baseURL: "https://tomatoestimer.herokuapp.com/api/",
+    // baseURL: "http://localhost:3300/api/",
   });
 };
