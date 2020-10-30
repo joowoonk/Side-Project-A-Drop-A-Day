a68b4734347d3cb89e72c09f185c4e2a01dada8f
Your Name <joowoonk@gmail.com>
2020-10-30 16:06:01 -0700

    disabled project nav as well

--- a-drop-a-day/src/components/nav/nav.component.js
+++ a-drop-a-day/src/components/nav/nav.component.js
@@ -105,7 +105,10 @@ const Nav = ({ isStopping, setIsStopping }) => {
               className="navMenu"
               alt="projects"
               onClick={() => {
-                push("/projects");
+                if (isStopping) {
+                  push("/projects");
+                }
+               
               }}
             >
               PROJECTS
